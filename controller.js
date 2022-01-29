3bac750d7988851360f9924a2003a0ad7755be74
Topan Satria Effendy <topansatriaeffendy@gmail.com>
2022-01-29 21:34:23 +0700

    Menambahkan routes

---
+++ controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var ress = require('./res');
+var conn = require('./koneksi');
+
+exports.index = function(req, res) {
+    ress.ok('Respone Sukses')
+}
---
+++ routes.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function(app) {
+    var json = require('./controller');
+    app.route('/').get(json.index);
+}
