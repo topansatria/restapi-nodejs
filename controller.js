c0a931da5a4b46b42d7f7d6a25720ab2936415c2
Topan Satria Effendy <topansatriaeffendy@gmail.com>
2022-01-29 22:05:50 +0700

    Menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -2,7 +2,20 @@
 
 var ress = require('./res');
 var conn = require('./koneksi');
+const e = require('express');
+const Connection = require('mysql/lib/Connection');
 
 exports.index = function(req, res) {
     ress.ok('Respone Sukses', res)
+}
+
+// menampilkan semua data mahasiswa
+exports.tampilSemuaData = function(req, res) {
+    conn.query('SELECT * FROM tbl_mahasiswa', function(err, rows, fileds) {
+        if (err) {
+            conn.log(err);
+        } else {
+            ress.ok(rows, res);
+        }
+    });
 }
--- routes.js
+++ routes.js
@@ -2,5 +2,10 @@
 
 module.exports = function(app) {
     var json = require('./controller');
+
+    // routes index
     app.route('/').get(json.index);
+
+    // routes tampil semua data mahasiswa
+    app.route('/tampil').get(json.tampilSemuaData);
 }
